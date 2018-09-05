babc508f3820b722520d30038d03950e8afd1d2e
AsiaClarkson <aclarkso@usc.edu>
2018-09-05 12:56:47 -0700

    Finished Assignment 2 w/ Handlebars capabilities and images this time!

--- main.js
+++ main.js
@@ -1,39 +1,43 @@
-$('#searchbutton').on('click', function(event){
+let postTemplateString = document.querySelector('#post-template').innerHTML;
+let renderPosts = Handlebars.compile(postTemplateString);
+Handlebars.registerHelper('withCommas', function (subs) {
+  return subs.toLocaleString();
+});
+
+//Search box functionality & requesting from the right page
+$('#searchbutton').on('click', function (event) {
   event.preventDefault(); //gets rid of the pound sign for anchor tags
   let loader = '<div class="loader">Loading...</div>';
   let beginningurl = 'https://www.reddit.com/r/';
   let usersearch = $('#searchbox').val();
   let endurl = '.json'
   let url = beginningurl + usersearch + endurl;
+
+
+
+
   console.log(url);
-  $('#postdata').html(loader);
+  $('#allposts').html(loader);
+
   let promise = $.ajax({
     type: 'GET',
     url: url
   });
 
-  promise.then(function(titles){
-    console.log('success', titles);
-    let html = '';
-    titles.data.children.forEach(function(title){
-      html += `
-      <div id='post'>
-        <span id='posttitle'><a href='${title.data.url}'>${title.data.title}</a></span>
-        <br><br>
-        <img id='postimg' src='${title.data.thumbnail}'>
-        <br>
-        <span id='score'> ${title.data.score}</span>
-        <span id='author'>${title.data.author}</span>
-        <br>
-        </div>
-        <br>
-      `;
-    });
-    $('#postdata').html(html);
+  promise.then(function (response) {
+      console.log('success', response);
+      console.log('help');
+      let renderedPosts = renderPosts({
+        posts: response.data.children
+      });
+      console.log(renderedPosts);
+      $('#allposts').html(renderedPosts);
 
-  },
-  function(){
-    console.log('error',error);//checking for errors
-  });
+    },
+
+    function (error) {
+      console.log('error', error);
+      $("#allposts").html('<span id="oops">Houston, we have an error!</span>'); //checking for errors
+    })
 
 });
